e95e8aff488219a33851768edfb557069e6ecc35
achou11 <andrew.chou@yahoo.com>
2017-09-21 12:38:18 -0500

    Message after losing tells user correct word

--- app.js
+++ app.js
@@ -174,7 +174,8 @@ function enterGuess() {
 
     // User runs out of lives
     if (lives == 0) {
-        document.getElementById('win-lose').innerHTML = 'Game over. You lost.<br/>But at least you got away :)';
+        document.getElementById('win-lose').innerHTML =
+        'Game over. The correct answer was ' + targetWord '.<br>But hey, at least you got away :)';
         var newScoreLose = changeScore(false);
         updateUserScore(newScoreLose);
     }
